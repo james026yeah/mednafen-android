6a2dd16132fe587be0e2356b83477a87788e654d
Ruby <ruby.dongyu@gmail.com>
2016-05-30 16:50:32 +0800

    init

---
+++ jni/Application.mk
@@ -0,0 +1,11 @@
+
+# Uncomment this if you're using STL in your project
+# See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
+#APP_STL := stlport_static 
+#APP_PLATFORM := android-12
+APP_STL := stlport_static
+STLPORT_FORCE_REBUILD := true
+
+
+APP_ABI := armeabi
+#APP_ABI := armeabi armeabi-v7a mips x86
---
+++ jni/src/Android.mk
@@ -0,0 +1,29 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := main
+
+SDL_PATH := ../SDL
+MEDNAFEN_SRC_PATH := $(LOCAL_PATH)/Mednafen/src
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include
+LOCAL_CPP_INCLUDES := $(LOCAL_PATH)
+LOCAL_CPP_INCLUDES += $(LOCAL_PATH)/Mednafen/src
+
+
+# Add your application source files here...
+LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
+	 $(MEDNAFEN_SRC_PATH)/drivers_android/main.c
+#	 testgles.c \
+
+
+#LOCAL_SRC_FILES += Mednafen/src/drivers_android/main.cpp
+
+LOCAL_SHARED_LIBRARIES := SDL2
+
+LOCAL_STATIC_LIBRARIES := stlport_static
+
+LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog
+
+include $(BUILD_SHARED_LIBRARY)
